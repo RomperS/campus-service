d05a43ef9c266ae32728d75df8d9bd4421ba8c25
Oswaldo Lopez <77045040+RomperS@users.noreply.github.com>
2025-09-14 09:15:06 +0000

    refactor(domain): change updateclassroomport to use updatedclassroomcommand

--- src/main/java/com/olo/campusservice/domain/port/inbound/classroom/UpdateClassroomPort.java
+++ src/main/java/com/olo/campusservice/domain/port/inbound/classroom/UpdateClassroomPort.java
@@ -1,7 +1,8 @@
 package com.olo.campusservice.domain.port.inbound.classroom;
 
+import com.olo.campusservice.domain.command.UpdatedClassroomCommand;
 import com.olo.campusservice.domain.model.Classroom;
 
 public interface UpdateClassroomPort {
-    Classroom updateClassroom(Classroom classroom);
+    Classroom updateClassroom(UpdatedClassroomCommand classroom);
 }
