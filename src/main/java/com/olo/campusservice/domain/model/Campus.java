eba0318645764ec5b1479fdbaa61a10c6145e312
Oswaldo Lopez <77045040+RomperS@users.noreply.github.com>
2025-09-14 05:29:04 +0000

    refactor(domain): remove classroomidentifier list and make relation directional

--- src/main/java/com/olo/campusservice/domain/model/Campus.java
+++ src/main/java/com/olo/campusservice/domain/model/Campus.java
@@ -1,11 +1,8 @@
 package com.olo.campusservice.domain.model;
 
-import java.util.List;
-
 public record Campus(
         Long id,
         String name,
-        String address,
-        List<String> classroomsIdentifiers
+        String address
 ) {
 }
