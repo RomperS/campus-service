6e50e08fb265487e34ca236291fc5c2ecd748ae4
Oswaldo Lopez <77045040+RomperS@users.noreply.github.com>
2025-09-14 08:50:25 +0000

    feat(domain): add findallresourceport implementation

---
+++ src/main/java/com/olo/campusservice/application/usecase/resource/FindAllResourceImpl.java
@@ -0,0 +1,20 @@
+package com.olo.campusservice.application.usecase.resource;
+
+import com.olo.campusservice.domain.model.Resource;
+import com.olo.campusservice.domain.port.inbound.resource.FindAllResourcePort;
+import com.olo.campusservice.domain.port.outbound.CampusResourceRepository;
+import com.olo.campusservice.domain.port.outbound.ResourceRepository;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+public class FindAllResourceImpl implements FindAllResourcePort {
+
+    private final ResourceRepository resourceRepository;
+
+    @Override
+    public List<Resource> findAll() {
+        return resourceRepository.findAll();
+    }
+}
