6370655824bbc7f361bfbcb611b5c48353406b7c
Oswaldo Lopez <77045040+RomperS@users.noreply.github.com>
2025-09-14 05:46:32 +0000

    feat(domain): add shifts set to campus model

--- src/main/java/com/olo/campusservice/domain/model/Campus.java
+++ src/main/java/com/olo/campusservice/domain/model/Campus.java
@@ -1,8 +1,13 @@
 package com.olo.campusservice.domain.model;
 
+import com.olo.campusservice.domain.model.enums.Shift;
+
+import java.util.Set;
+
 public record Campus(
         Long id,
         String name,
-        String address
+        String address,
+        Set<Shift> shifts
 ) {
 }
