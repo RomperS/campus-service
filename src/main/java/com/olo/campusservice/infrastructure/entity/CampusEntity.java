be4a23defc452729088f19fe43aae99d0949dfb4
Oswaldo Lopez <77045040+RomperS@users.noreply.github.com>
2025-09-14 22:03:53 +0000

    feat(campus): add CampusEntity and liquibase changelogs (master and 01-create-campus-table)

---
+++ src/main/java/com/olo/campusservice/infrastructure/entity/CampusEntity.java
@@ -0,0 +1,35 @@
+package com.olo.campusservice.infrastructure.entity;
+
+import com.olo.campusservice.domain.model.enums.Shift;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Table(name = "campuses")
+public class CampusEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String address;
+
+    @ElementCollection(targetClass = Shift.class, fetch = FetchType.EAGER)
+    @CollectionTable(
+            name = "campus_shifts",
+            joinColumns = @JoinColumn(name = "campus_id")
+    )
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "shift")
+    private Set<Shift> shifts = new HashSet<>();
+}
