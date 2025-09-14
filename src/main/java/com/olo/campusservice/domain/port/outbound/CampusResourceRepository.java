3590e663aab269c21b3f3bed5f8edb0c15bc99ad
Oswaldo Lopez <77045040+RomperS@users.noreply.github.com>
2025-09-14 09:06:27 +0000

    feat(domain): add methods to campusresourcerepository interface

--- src/main/java/com/olo/campusservice/domain/port/outbound/CampusResourceRepository.java
+++ src/main/java/com/olo/campusservice/domain/port/outbound/CampusResourceRepository.java
@@ -1,4 +1,20 @@
 package com.olo.campusservice.domain.port.outbound;
 
+import com.olo.campusservice.domain.model.Campus;
+import com.olo.campusservice.domain.model.CampusResource;
+import com.olo.campusservice.domain.model.Resource;
+import com.olo.campusservice.domain.model.enums.State;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface CampusResourceRepository {
+    boolean existsByCampusAndResourceAndState(Campus campus, Resource resource, State state);
+
+    CampusResource save(CampusResource newCampusResource);
+
+    List<CampusResource> findAll();
+    Optional<CampusResource> findById(Long id);
+
+    void delete(Long id);
 }
