ba27d35efadce4ca60f400d0fabc0ab4a1bad0f0
Oswaldo Lopez <77045040+RomperS@users.noreply.github.com>
2025-09-14 05:56:51 +0000

    feat(domain): add methods to classroomrepository interface

--- src/main/java/com/olo/campusservice/domain/port/outbound/ClassroomRepository.java
+++ src/main/java/com/olo/campusservice/domain/port/outbound/ClassroomRepository.java
@@ -1,4 +1,20 @@
 package com.olo.campusservice.domain.port.outbound;
 
+import com.olo.campusservice.domain.model.Campus;
+import com.olo.campusservice.domain.model.Classroom;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface ClassroomRepository {
+    boolean existsByIdentifierInCampus(String identifier, Campus campus);
+
+    Classroom save(Classroom classroom);
+
+    Classroom findAll();
+    Optional<Classroom> findById(Long id);
+    List<Classroom> findByIdentifier(String identifier);
+    List<Classroom> findByCampus();
+
+    void deleteById(Long id);
 }
