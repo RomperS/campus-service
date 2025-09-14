7e849113ff42b6888ae97285a23ca9a5ffe1d45e
Oswaldo Lopez <77045040+RomperS@users.noreply.github.com>
2025-09-14 08:32:12 +0000

    feat(domain): add methods to resourcerepository interface

--- src/main/java/com/olo/campusservice/domain/port/outbound/ResourceRepository.java
+++ src/main/java/com/olo/campusservice/domain/port/outbound/ResourceRepository.java
@@ -1,4 +1,21 @@
 package com.olo.campusservice.domain.port.outbound;
 
+import com.olo.campusservice.domain.model.Resource;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface ResourceRepository {
+    boolean existsById(Long id);
+    boolean existsByName(String name);
+
+    Resource save(Resource resource);
+
+    Optional<Resource> findById(Long id);
+
+    List<Resource> findByName(String name);
+    List<Resource> findAll();
+    List<Resource> findBySupplier(String name);
+
+    void delete(Long id);
 }
