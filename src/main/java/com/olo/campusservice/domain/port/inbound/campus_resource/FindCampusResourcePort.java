7b339f51931b35a06c016bf03a90044ec405e2b2
Oswaldo Lopez <77045040+RomperS@users.noreply.github.com>
2025-09-14 08:56:06 +0000

    fix(domain): change findcampusresourceport return type to list

--- src/main/java/com/olo/campusservice/domain/port/inbound/campus_resource/FindCampusResourcePort.java
+++ src/main/java/com/olo/campusservice/domain/port/inbound/campus_resource/FindCampusResourcePort.java
@@ -3,6 +3,8 @@ package com.olo.campusservice.domain.port.inbound.campus_resource;
 import com.olo.campusservice.domain.command.SearchCampusResourceCommand;
 import com.olo.campusservice.domain.model.CampusResource;
 
+import java.util.List;
+
 public interface FindCampusResourcePort {
-    CampusResource find(SearchCampusResourceCommand command);
+    List<CampusResource> find(SearchCampusResourceCommand command);
 }
