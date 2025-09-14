820f14b1c325e1a44141b922385d93760394cf23
Oswaldo Lopez <77045040+RomperS@users.noreply.github.com>
2025-09-14 08:57:43 +0000

    refactor(domain): change state field in searchcampusresourcecommand from optional to enum

--- src/main/java/com/olo/campusservice/domain/command/SearchCampusResourceCommand.java
+++ src/main/java/com/olo/campusservice/domain/command/SearchCampusResourceCommand.java
@@ -2,13 +2,11 @@ package com.olo.campusservice.domain.command;
 
 import com.olo.campusservice.domain.model.enums.State;
 
-import java.util.Optional;
-
 public record SearchCampusResourceCommand(
         Long id,
         Long campusId,
         Long resourceId,
         int quantity,
-        Optional<State> state
+        State state
 ) {
 }
