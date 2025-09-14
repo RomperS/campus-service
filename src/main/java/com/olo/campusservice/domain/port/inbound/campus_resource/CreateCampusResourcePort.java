2cc75b06cb7d0ae04bfd9ab8a0f27a547b7c1603
Oswaldo Lopez <77045040+RomperS@users.noreply.github.com>
2025-09-14 09:19:36 +0000

    refactor(domain): change createcampusresourceport to use campusresourcecommand

--- src/main/java/com/olo/campusservice/domain/port/inbound/campus_resource/CreateCampusResourcePort.java
+++ src/main/java/com/olo/campusservice/domain/port/inbound/campus_resource/CreateCampusResourcePort.java
@@ -1,7 +1,8 @@
 package com.olo.campusservice.domain.port.inbound.campus_resource;
 
+import com.olo.campusservice.domain.command.CampusResourceCommand;
 import com.olo.campusservice.domain.model.CampusResource;
 
 public interface CreateCampusResourcePort {
-    CampusResource create(CampusResource campusResource);
+    CampusResource create(CampusResourceCommand command);
 }
