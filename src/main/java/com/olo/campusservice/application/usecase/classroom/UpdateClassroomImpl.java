2dd0c82bcbcc719d1f7cd320d39f423afe400bb0
Oswaldo Lopez <77045040+RomperS@users.noreply.github.com>
2025-09-14 09:16:13 +0000

    refactor(domain): update updateclassroomimpl to use updatedclassroomcommand

--- src/main/java/com/olo/campusservice/application/usecase/classroom/UpdateClassroomImpl.java
+++ src/main/java/com/olo/campusservice/application/usecase/classroom/UpdateClassroomImpl.java
@@ -1,5 +1,6 @@
 package com.olo.campusservice.application.usecase.classroom;
 
+import com.olo.campusservice.domain.command.UpdatedClassroomCommand;
 import com.olo.campusservice.domain.exception.exists.ClassroomIdentifierTakenException;
 import com.olo.campusservice.domain.exception.exists.ClassroomNotFoundException;
 import com.olo.campusservice.domain.model.Classroom;
@@ -13,20 +14,20 @@ public class UpdateClassroomImpl implements UpdateClassroomPort {
     private final ClassroomRepository classroomRepository;
 
     @Override
-    public Classroom updateClassroom(Classroom classroom) {
-        Classroom targetClassroom = classroomRepository.findById(classroom.id()).orElseThrow(() -> new ClassroomNotFoundException("Classroom not found"));
+    public Classroom updateClassroom(UpdatedClassroomCommand command) {
+        Classroom targetClassroom = classroomRepository.findById(command.id()).orElseThrow(() -> new ClassroomNotFoundException("Classroom not found"));
 
         String identifier = targetClassroom.identifier();
-        if (classroom.identifier() != null && !classroom.identifier().equals(identifier)) {
-            if (classroomRepository.existsByIdentifierInCampus(classroom.identifier(), targetClassroom.campus())){
+        if (command.identifier() != null && !command.identifier().equals(identifier)) {
+            if (classroomRepository.existsByIdentifierInCampus(command.identifier(), targetClassroom.campus())){
                 throw new ClassroomIdentifierTakenException("Identifier is already taken");
             }
-            identifier = classroom.identifier();
+            identifier = command.identifier();
         }
 
         Integer capacity = targetClassroom.capacity();
-        if (classroom.capacity() > 0){
-            capacity = classroom.capacity();
+        if (command.capacity() > 0){
+            capacity = command.capacity();
         }
 
         Classroom updatedClassroom = new Classroom(
