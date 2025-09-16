e6c76585239ed34b206bd9ac0df456ca9b5707fd
Oswaldo Lopez <77045040+RomperS@users.noreply.github.com>
2025-09-16 21:28:57 +0000

    refactor(kafka-service-port): replace generic publish methods with specific command types

--- src/main/java/com/olo/campusservice/domain/port/outbound/KafkaServicePort.java
+++ src/main/java/com/olo/campusservice/domain/port/outbound/KafkaServicePort.java
@@ -1,9 +1,14 @@
 package com.olo.campusservice.domain.port.outbound;
 
-import com.olo.campusservice.domain.command.PublishCommand;
+import com.olo.campusservice.domain.command.kafka.PublishClassroomCommand;
+import com.olo.campusservice.domain.command.kafka.PublishCreateCampusCommand;
+import com.olo.campusservice.domain.command.kafka.PublishDeleteCommand;
+import com.olo.campusservice.domain.command.kafka.PublishShiftCommand;
 
 public interface KafkaServicePort {
-    void publishCreate(PublishCommand command);
-    void publishUpdateClassroom(PublishCommand command);
-    void publishDelete(PublishCommand command);
+    void publishCreateClassroom(PublishClassroomCommand command);
+    void publishCreateCampus(PublishCreateCampusCommand command);
+    void publishUpdateClassroom(PublishClassroomCommand command);
+    void publishRemoveShift(PublishShiftCommand command);
+    void publishDelete(PublishDeleteCommand command);
 }
