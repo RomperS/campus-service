056742008a5466930acaadf7ca6e50d60f51022c
Oswaldo Lopez <77045040+RomperS@users.noreply.github.com>
2025-09-17 08:04:40 +0000

    feat(exception): add SecurityException for security related errors

---
+++ src/main/java/com/olo/campusservice/domain/exception/base/SecurityException.java
@@ -0,0 +1,11 @@
+package com.olo.campusservice.domain.exception.base;
+
+public class SecurityException extends RuntimeException {
+    public SecurityException(String message) {
+        super(message);
+    }
+
+    public SecurityException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
